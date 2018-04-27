c341520fdcf6950ebb69c4dfed1b600d18b1a15e
zhoujiacan <zhoujiacan@sanduspace.cn>
2018-04-27 14:12:42 +0800

    change something on yml

---
+++ login-server/src/main/java/com/zjc/learn/loginserver/controller/controller.java
@@ -0,0 +1,12 @@
+package com.zjc.learn.loginserver;
+
+/**
+ * CopyRight (c) 2018 Sandu Technology Inc.
+ * <p>
+ * Spring-Cloud
+ *
+ * @author Sandu
+ * @datetime 2018/4/26 9:57
+ */
+public class controller {
+}
