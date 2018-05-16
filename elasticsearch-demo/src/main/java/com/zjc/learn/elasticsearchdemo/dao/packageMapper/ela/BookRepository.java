79ec9dd4943e0f744234d49179cce9678ba58520
zhoujiacan <zhoujiacan@sanduspace.cn>
2018-05-16 14:57:56 +0800

    elasticsearch querySearch and elasticsearchRepository search

--- elasticsearch-demo/src/main/java/com/zjc/learn/elasticsearchdemo/dao/packageMapper/ela/BookRepository.java
+++ elasticsearch-demo/src/main/java/com/zjc/learn/elasticsearchdemo/dao/packageMapper/ela/BookRepository.java
@@ -4,6 +4,7 @@ import com.zjc.learn.elasticsearchdemo.model.packageModel.Book;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface BookRepository extends ElasticsearchRepository<Book,String> {
 
@@ -13,6 +14,10 @@ public interface BookRepository extends ElasticsearchRepository<Book,String> {
 
     List<Book> findAllByAuthorAndContentLike(String author, String span);
 
-    List<Book> findByContentLike(String span);
+    List<Book> findByNameLike(String span);
+
+    Stream<Book> findByNameLikeAndContent(String name,String content);
+
+    Stream<Book> findByNameLikeOrAuthorLike(String name,String author);
 
 }
--- elasticsearch-demo/src/test/java/com/zjc/learn/elasticsearchdemo/ElasticsearchDemoApplicationTests.java
+++ elasticsearch-demo/src/test/java/com/zjc/learn/elasticsearchdemo/ElasticsearchDemoApplicationTests.java
@@ -6,13 +6,23 @@ import com.zjc.learn.elasticsearchdemo.dao.packageMapper.ela.SysUserElaDao;
 import com.zjc.learn.elasticsearchdemo.model.packageModel.Book;
 import com.zjc.learn.elasticsearchdemo.model.packageModel.SysUser;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.QueryStringQueryBuilder;
+import org.hibernate.cfg.annotations.QueryBinder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.xml.bind.SchemaOutputResolver;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,6 +37,7 @@ public class ElasticsearchDemoApplicationTests {
 
     @Autowired
     private BookRepository bookRepository;
+
     @Test
     public void contextLoads() {
         SysUser sysUser = new SysUser();
@@ -37,19 +48,40 @@ public class ElasticsearchDemoApplicationTests {
         System.out.println(sysUser);
 
 //        elaDao.save(sysUser);
-        bookRepository.save(Book.builder().id(12+"").author("张三").name("Think in Java").content("test").build());
+        bookRepository.save(Book.builder().id(12 + "").author("张三").name("Think in Python").content("test").build());
     }
 
     @Test
-    public void  find(){
+    public void find() {
         bookRepository.deleteById("12");
 //        Book book = bookRepository.findFirstByAuthor("张三");
         List<Book> books = bookRepository.findAllByAuthor("张三");
         List<Book> allByAuthorAndContentLike = bookRepository.findAllByAuthorAndContentLike("张三", "es");
-        List<Book> es = bookRepository.findByContentLike("es");
-        log.info("es us []",es);
+        List<Book> es = bookRepository.findByNameLike("ja");
+        Stream<Book> byNameLikeAndContent = bookRepository.findByNameLikeAndContent("ja", "test");
+        List<Book> collect = byNameLikeAndContent.collect(Collectors.toList());
+        log.info("es us []", es);
+
         System.out.println(allByAuthorAndContentLike);
         System.out.println(books);
+    }
+
+    @Test
+    public void doQuerySearch() {
+        BoolQueryBuilder must = QueryBuilders.boolQuery();
+        must.must(QueryBuilders.fuzzyQuery("name", "java")).must(QueryBuilders.matchQuery("content","test"));
+        Iterable<Book> rets = bookRepository.search(QueryBuilders.fuzzyQuery("content", "es"));
+        rets = bookRepository.search(QueryBuilders.queryStringQuery("test"));
+        rets = bookRepository.search(QueryBuilders.queryStringQuery("Think"));
+        rets = bookRepository.search(QueryBuilders.termQuery("author", "张三"));
+        rets = bookRepository.search(QueryBuilders.termQuery("author", "张"));
+        rets = bookRepository.search(QueryBuilders.termQuery("author", "三"));
+        rets = bookRepository.search(QueryBuilders.spanTermQuery("author", "张三"));
+        rets = bookRepository.search(QueryBuilders.fuzzyQuery("name", "Java"));
+        rets = bookRepository.search(QueryBuilders.fuzzyQuery("name", "in"));
+        rets = bookRepository.search(must);
+        rets.forEach(System.out::println);
+
     }
 
 }
